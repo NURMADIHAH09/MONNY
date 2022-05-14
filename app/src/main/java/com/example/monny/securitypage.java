fe0d727860d38a4f912ebb3cb3ad44bee2bc194a
fitrahh18 <fitrahbahsri01@gmail.com>
2022-05-15 00:30:44 +0800

    latest
    latestinterface

---
+++ app/src/main/java/com/example/monny/securitypage.java
@@ -0,0 +1,14 @@
+package com.example.monny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class securitypage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_securitypage);
+    }
+}
